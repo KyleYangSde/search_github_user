41bb4de3c99f185d170171b91acf27ae0f6b9849
kyle <1063683271@qq.com>
2020-02-23 21:51:51 +1100

    first commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+//callback when doc finish loading
+$(document).ready(function() {
+  //当发生 keydown 和 keyup 事件时，输入域会改变颜色
+  $("#searchUser").on("keyup", function(e) {
+    //获取输入值
+    let username = e.target.value;
+
+    //make request to github
+    $.ajax({
+      url: "https://api.github.com/users/" + username,
+      data: {
+        client_id: "1891b1840403144e50a7",
+        client_secret: "c39efff825e32f7ac8e17e37e597c0121cbf9d4f",
+        sort: "created:asc",
+        per_page: 5
+      }
+      //return promise; a callback function
+    }).done(function(user) {
+      $.ajax({
+        url: "https://api.github.com/users/" + username + "/repos",
+        data: {
+          client_id: "1891b1840403144e50a7",
+          client_secret: "c39efff825e32f7ac8e17e37e597c0121cbf9d4f"
+        }
+      }).done(function(repos) {
+        //console.log(repos);
+        $.each(repos, function(index, repo) {
+          $("#repos").append(`
+              <div class="well>
+                <div class="row">
+                    <div class="col-md-7">
+                        <strong>${repo.name}</strong>:{repo.description}
+                    </div>
+                    <div class="col-md-3">
+                        <span class="label label-default">Forks: ${repo.forks_count}</span>
+                        <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
+                        <span class="label label-success">Stars: ${user.stargazers_count}</span>
+                    </div>
+                    <div class="col-md-2">
+                        <a href="${repo.html_url}" target="_blank" class="btn btn-default">access</a>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
+      });
+      // html() 方法返回或设置被选元素的内容 (inner HTML)。
+      $("#profile").html(`
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">${user.name}</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img  class="thumbnail avatar" src="${user.avatar_url}">
+                        <a class="btn btn-primary btn-block" target= "_blank" href="${user.html_url}">View Profile</a>
+                    </div>
+                    <div class="col-md-9">
+                        <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                        <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                        <span class="label label-success">Followers: ${user.followers}</span>
+                        <span class="label label-info">Following: ${user.following}</span>
+                        <br><br>
+                        <ul class="list-group">
+                            <li class="list-group-item">Company:${user.company}</li>
+                            <li class="list-group-item">Website/blog:${user.blog}</li>
+                            <li class="list-group-item">Location:${user.location}</li>
+                            <li class="list-group-item">Menber Since:${user.created_at}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h3 class="page-header">Latest Repos</h3>
+        <div id="repos"></div>
+      `);
+    });
+  });
+});
